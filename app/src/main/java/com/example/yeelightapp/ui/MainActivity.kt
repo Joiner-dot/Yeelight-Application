8f2d917160afca53883c0d76ee05b2434608a72a
Egor Kuzmin1 <egor_kuzmin1@epam.com>
2021-05-11 09:41:33 +0300

    New Feature

--- app/src/main/java/com/example/yeelightapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/yeelightapp/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import com.example.yeelightapp.R
 import com.example.yeelightapp.di.appModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 
@@ -15,12 +16,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar!!.hide();
+        startKoin {
+            androidContext(applicationContext)
+            modules(appModule)
+        }
         AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startKoin {
-            modules(appModule)
-        }
     }
 
     override fun onDestroy() {
