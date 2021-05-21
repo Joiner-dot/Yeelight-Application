54142fc9185c89a6d2348bde59827a98d66e5f0c
Egor Kuzmin1 <egor_kuzmin1@epam.com>
2021-05-21 14:45:44 +0300

    YA-fixing view mode

--- app/src/main/java/com/example/yeelightapp/ui/RestartTools.kt
+++ app/src/main/java/com/example/yeelightapp/ui/RestartTools.kt
@@ -1,18 +1,11 @@
 package com.example.yeelightapp.ui
 
-import android.app.Activity
 import android.content.Context
-import android.content.Intent
+import com.jakewharton.processphoenix.ProcessPhoenix
 
 class RestartTools(private val context: Context) {
 
     fun restartApp(){
-        val intent = Intent(context, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        context.startActivity(intent)
-        if (context is Activity) {
-            context.finish()
-        }
-        Runtime.getRuntime().exit(0)
+        ProcessPhoenix.triggerRebirth(context)
     }
 }
