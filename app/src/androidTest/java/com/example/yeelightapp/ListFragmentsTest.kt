9a1c94e057986349ce6ad01c0cdd87783948e989
Egor Kuzmin1 <egor_kuzmin1@epam.com>
2021-05-28 11:04:00 +0300

    YA testing new features

--- app/src/androidTest/java/com/example/yeelightapp/ListFragmentsTest.kt
+++ app/src/androidTest/java/com/example/yeelightapp/ListFragmentsTest.kt
@@ -1,47 +1,27 @@
 package com.example.yeelightapp
 
-import android.app.Instrumentation
-import android.content.Intent
-import android.os.Bundle
-import android.text.Editable
-import android.widget.EditText
-import androidx.core.content.ContextCompat.startActivity
+
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.MutableLiveData
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.NoMatchingViewException
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.yeelightapp.ui.MainActivity
+import com.example.yeelightapp.ui.fragments.AddLamp
+import com.example.yeelightapp.ui.fragments.ListFragments
+import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.Assert.*
 
-
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
-import com.example.yeelightapp.lamps.LampDB
-import com.example.yeelightapp.lamps.LampUI
-import com.example.yeelightapp.mapper.LampMapper
-import com.example.yeelightapp.ui.LampActivity
-import com.example.yeelightapp.ui.MainActivity
-import com.example.yeelightapp.ui.fragments.AboutPage
-import com.example.yeelightapp.ui.fragments.AddLamp
-import com.example.yeelightapp.ui.fragments.ListFragments
-import com.example.yeelightapp.ui.fragments.ManageStaticLight
-import com.robotium.solo.Solo
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import org.koin.core.definition.Options
 
 @RunWith(AndroidJUnit4::class)
 class ListFragmentTest {
@@ -104,14 +84,17 @@ class ListFragmentTest {
     @Test
     fun selectButtonCheck() {
         val titleScenario = launchFragmentInContainer<AddLamp>()
-
         titleScenario.onFragment { fragment ->
-            navController.setGraph(R.navigation.my_nav)
-
+            assertNotNull(fragment.activity)
+            val navController = TestNavHostController(fragment.activity!!)
+            fragment.activity!!.runOnUiThread { navController.setGraph(R.navigation.my_nav) }
             Navigation.setViewNavController(fragment.requireView(), navController)
+            navController.navigate(R.id.action_listFragments_to_addLamp)
+            var currentDestination = navController.currentDestination
+            assertEquals(currentDestination?.id, R.id.addLamp)
+            navController.navigate(R.id.action_addLamp_to_listFragments)
+            currentDestination = navController.currentDestination
+            assertEquals(currentDestination?.id, R.id.listFragments)
         }
-        onView(withId(R.id.select))
-            .perform(click())
-        assertEquals(navController.currentDestination?.id, R.id.listFragments)
     }
 }
--- app/src/main/java/com/example/yeelightapp/ui/RestartTools.kt
+++ app/src/main/java/com/example/yeelightapp/ui/RestartTools.kt
@@ -1,19 +1,29 @@
 package com.example.yeelightapp.ui
 
 import android.content.Context
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import com.jakewharton.processphoenix.ProcessPhoenix
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
-class RestartTools(private val context: Context) {
+
+open class RestartTools(private val context: Context) {
 
     suspend fun restartApp() {
-        Toast.makeText(
-            context,
-            "Restarting Application",
-            Toast.LENGTH_SHORT
-        ).show()
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(
+                context,
+                "Restarting Application",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
         delay(1000)
-        ProcessPhoenix.triggerRebirth(context)
+        val intent = Intent(context, MainActivity::class.java)
+        ProcessPhoenix.triggerRebirth(context, intent)
     }
 }
